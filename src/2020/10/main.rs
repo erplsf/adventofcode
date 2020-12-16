6ac8e4d73f8bacae118815d857d1d0170ba16528
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-12-16 14:52:50 +0100

    solve 2020-10-2

--- src/2020/10/main.rs
+++ src/2020/10/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
 use adventofcode::lib::parse_file;
+use std::collections::HashMap;
 
-fn find_lefts(chargers: Vec<usize>) -> Vec<usize> {
+fn find_lefts(chargers: &Vec<usize>) -> Vec<usize> {
     let mut lefts: Vec<usize> = vec![];
-    for triple in chargers.windows(3).rev() {
+    for triple in chargers.windows(3) {
         let f = triple.first().unwrap();
         let s = triple[1];
         let t = triple.last().unwrap();
@@ -13,11 +13,34 @@ fn find_lefts(chargers: Vec<usize>) -> Vec<usize> {
         let d = t - f;
         // idea: if d < 3 then take the next number and check if we can throw it out too
         if d < 3 {
-            dbg!(t, s, f);
+            // dbg!(t, s, f);
             lefts.push(s);
         }
     }
     lefts
+}
+
+fn split_in_ranges(chargers: &Vec<usize>) -> Vec<Vec<usize>> {
+    let mut ranges: Vec<Vec<usize>> = vec![];
+    let mut current_range = vec![];
+    for double in chargers.windows(2) {
+        let f = double.first().unwrap();
+        let s = double.last().unwrap();
+        let d = s - f;
+        if d == 1 {
+            current_range.push(*f);
+            current_range.push(*s);
+        } else {
+            // dbg!(*s, double);
+            current_range.dedup();
+            ranges.push(current_range);
+            current_range = vec![];
+            current_range.push(*s);
+        }
+    }
+    current_range.dedup();
+    ranges.push(current_range);
+    ranges
 }
 
 fn main() {
@@ -35,9 +58,23 @@ fn main() {
         *count += 1
     }
     let result = diffs.get(&1).unwrap() * diffs.get(&3).unwrap();
-    let lefts = find_lefts(chargers);
-    dbg!(&diffs);
-    dbg!(&lefts);
+    let lefts = find_lefts(&chargers);
+    let ranges = split_in_ranges(&lefts);
     // dbg!(&chargers);
     dbg!(&result);
+    // dbg!(&chargers);
+    // dbg!(&lefts);
+    // dbg!(&ranges);
+    let combs: usize = ranges
+        .iter()
+        .map(|range| {
+            match range.len() {
+                1 => 2,
+                2 => 4,
+                3 => 7,
+                _ => panic!("unsupported case!"),
+            }
+        })
+        .product();
+    dbg!(&combs);
 }
