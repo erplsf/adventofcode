ae08479b3c8989dc80228e1e278283e48490619a
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-12-18 19:40:30 +0100

    solve 2020-14

--- src/2020/13/main.rs
+++ src/2020/13/main.rs
@@ -5,7 +5,7 @@ fn parse_ids(ids: &str) -> Vec<usize> {
         if id != "x" {
             id.parse().ok()
         } else {
-            None
+            Some(0)
         }
     }).collect()
 }
@@ -17,16 +17,54 @@ fn find_next_departure_since(since: usize, schedules: &Vec<usize>) -> Vec<usize>
     }).collect()
 }
 
+// function extended_gcd(a, b)
+//     (old_r, r) := (a, b)
+//     (old_s, s) := (1, 0)
+//     (old_t, t) := (0, 1)
+
+//     while r ≠ 0 do
+//     quotient := old_r div r
+//     (old_r, r) := (r, old_r − quotient × r)
+//     (old_s, s) := (s, old_s − quotient × s)
+//     (old_t, t) := (t, old_t − quotient × t)
+
+//     output "Bézout coefficients:", (old_s, old_t)
+//     output "greatest common divisor:", old_r
+//     output "quotients by the gcd:", (t, s)
+
+fn extended_euclid(a: isize, b: isize) -> (isize, isize) {
+    let (mut old_r, mut r) = (a, b);
+    let (mut old_s, mut s) = (1, 0);
+    let (mut old_t, mut t) = (0, 1);
+
+    while r != 0 {
+        let quotient = old_r / r;
+        let prov = r;
+        r = old_r - quotient * prov;
+        old_r = prov;
+        let prov = s;
+        s = old_s - quotient * prov;
+        old_s = prov;
+        let prov = t;
+        t = old_t - quotient * prov;
+        old_t = prov;
+    }
+
+    (old_s, old_t)
+}
+
 fn main() {
     let contents = parse_file().unwrap();
     let mut iter = contents.lines();
     let start_time: usize = iter.next().unwrap().parse().unwrap();
     let schedules = parse_ids(iter.next().unwrap());
-    // dbg!(&start_time, &schedules);
-    let nearest_next_schedules = find_next_departure_since(start_time, &schedules);
-    let diff: Vec<usize> = nearest_next_schedules.iter().map(|bus| bus - start_time).collect();
-    let min_index = diff.iter().enumerate().min_by_key(|&(_, item)| item).unwrap();
-    let final_result = schedules[min_index.0] * min_index.1;
-    dbg!(&final_result);
-    // TODO: Solve part 2 with "System of linear Diophantine equations"
+    let en: Vec<_> = schedules.iter().enumerate().filter(|&(i, v)| *v != 0).collect();
+    dbg!(&start_time, en);
+    // let nearest_next_schedules = find_next_departure_since(start_time, &schedules);
+    // let diff: Vec<usize> = nearest_next_schedules.iter().map(|bus| bus - start_time).collect();
+    // let min_index = diff.iter().enumerate().min_by_key(|&(_, item)| item).unwrap();
+    // let final_result = schedules[min_index.0] * min_index.1;
+    // dbg!(&final_result);
+    // TODO: Solve part 2 with "Сhinese remainder theorem -> existence construction"
+    dbg!(extended_euclid(3, 4));
 }
--- src/2020/14/main.rs
+++ src/2020/14/main.rs
@@ -32,53 +32,77 @@ fn apply_mask(value: usize, mask: &str) -> usize {
     usize::from_str_radix(&zip, 2).unwrap()
 }
 
-fn apply_decode_mask(value: usize, mask: &str) -> Vec<usize> {
+fn apply_decode_mask(value: usize, mask: &str) -> Vec<String> {
     let binary_value = binarize(value);
-    let zip: String = mask.chars().zip(binary_value.chars()).map(|(mask_bit, value_bit)| {
+    let mem_addr: String = mask.chars().zip(binary_value.chars()).map(|(mask_bit, value_bit)| {
         if mask_bit == '0' {
             value_bit
         } else {
             mask_bit
         }
     }).collect();
-    // dbg!(&zip);
-    vec![]
+    expand_mem_addr(&mem_addr)
 }
 
 fn expand_mem_addr(mask: &str) -> Vec<String> {
     let mut addr = String::with_capacity(mask.len());
-    let mut addresses = vec![String::with_capacity(mask.len())];
-    for bit in mask.chars() {
+    let mut addresses = vec![];
+    let mut plain = true;
+    for (i, bit) in mask.chars().enumerate() {
         if bit == 'X' {
             // TODO:
+            addresses.extend(expand_mem_addr(&(addr.clone() + "0" + &mask[i+1..])));
+            addresses.extend(expand_mem_addr(&(addr.clone() + "1" + &mask[i+1..])));
+            plain = false;
+            break;
         } else {
             addr.push(bit);
         }
     }
-    addresses.push(addr);
+    if plain {
+        addresses.push(addr);
+    }
     addresses
 }
 
 fn main() {
-    // let contents = parse_file().unwrap();
-    // let mut mask: String = String::new();
-    // let mut mem: HashMap<usize, usize> = HashMap::new();
-    // for line in contents.lines() {
-    //     if line.contains("mask") {
-    //         mask = parse_mask_line(line);
-    //     } else if line.contains("mem") {
-    //         let (pos, value) = parse_mem_line(line);
-    //         let masked_value = apply_mask(value, &mask);
-    //         mem.insert(pos, masked_value);
-    //     } else {
-    //         panic!("unsupported line found!");
-    //     }
-    // }
-    // let sum: usize = mem.values().sum();
-    // dbg!(&sum);
+    let contents = parse_file().unwrap();
+    let mut mask: String = String::new();
+    let mut mem: HashMap<usize, usize> = HashMap::new();
+    for line in contents.lines() {
+        if line.contains("mask") {
+            mask = parse_mask_line(line);
+        } else if line.contains("mem") {
+            let (pos, value) = parse_mem_line(line);
+            let masked_value = apply_mask(value, &mask);
+            mem.insert(pos, masked_value);
+        } else {
+            panic!("unsupported line found!");
+        }
+    }
+    let sum: usize = mem.values().sum();
+    dbg!(&sum);
 
-    let mask = parse_mask_line("mask = 000000000000000000000000000000X1001X");
-    let (pos, value) = parse_mem_line("mem[42] = 100");    
-    // dbg!(mask, pos, value);
-    let res = apply_decode_mask(pos, &mask);
+    let mut mask: String = String::new();
+    let mut mem: HashMap<usize, usize> = HashMap::new();
+    for line in contents.lines() {
+        if line.contains("mask") {
+            mask = parse_mask_line(line);
+        } else if line.contains("mem") {
+            let (pos, value) = parse_mem_line(line);
+            let mem_positions = apply_decode_mask(pos, &mask);
+            for mem_position in mem_positions {
+                // dbg!(&mem_position);
+                // dbg!(&mem_position);
+                let mem_pos = usize::from_str_radix(&mem_position, 2).unwrap();
+                // dbg!(mem_pos);
+                mem.insert(mem_pos, value);
+            }
+            let masked_value = apply_mask(value, &mask);
+        } else {
+            panic!("unsupported line found!");
+        }
+    }
+    let sum: usize = mem.values().sum();
+    dbg!(&sum);
 }
