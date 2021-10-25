48019730b5c5c8fde7e1e101e9a460269bea57bb
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2021-10-25 12:16:22 +0200

    start cpp 2015

---
+++ cpp/2015/solutions/1/main.cpp
@@ -0,0 +1,53 @@
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string read_input(string path) {
+  ifstream file(path);
+  if (file) {
+    stringstream content;
+    content << file.rdbuf();
+    return content.str();
+  }
+  return "";
+}
+
+string resolve_input(int argc, char **argv) {
+  string path;
+  if (argc == 2) {
+    path = argv[1];
+  } else {
+    path = ("../../inputs/1.txt");
+  }
+  return path;
+}
+
+int main(int argc, char *argv[]) {
+  string path = resolve_input(argc, argv);
+  string content = read_input(path);
+
+  // sol
+  int floor = 0;
+  uint index = 0;
+  bool stop = false;
+  for (char &ch : content) {
+    if (ch == '(') {
+      floor++;
+    } else if (ch == ')') {
+      floor--;
+    }
+    index++;
+    if (floor == -1 && !stop) {
+      stop = true;
+      cout << index << "\n";
+    }
+  }
+
+  cout << floor << "\n";
+
+  return 0;
+}
