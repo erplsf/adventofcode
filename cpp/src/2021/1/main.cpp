61833171eaa2376f63109169226af0134dbbe145
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2021-12-01 09:19:22 +0100

    day 1

--- cpp/src/2021/1/main.cpp
+++ cpp/src/2021/1/main.cpp
@@ -1,5 +1,55 @@
+#include <fstream>
 #include <iostream>
+#include <numeric>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() { return 0; }
+uint count_increases(const vector<uint> &mes, uint window) {
+  uint count = 0;
+  if (window == 0) {
+    return count;
+  }
+
+  uint size = mes.size();
+  uint i = 0;
+  while ((i + window) <= size) {
+    auto s1 = mes.begin() + i;
+    auto e1 = mes.begin() + i + window;
+    auto s2 = mes.begin() + i + 1;
+    auto e2 = mes.begin() + i + 1 + window;
+
+    auto first = accumulate(s1, e1, 0, plus<uint>());
+    auto second = accumulate(s2, e2, 0, plus<uint>());
+
+    // cout << "first: " << first << "\n";
+    // cout << "second: " << second << "\n";
+
+    if (second - first > 0)
+      count++;
+    i++;
+  }
+
+  return count;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    return 1;
+  }
+  ifstream file(argv[1]);
+  string contents((std::istreambuf_iterator<char>(file)),
+                  std::istreambuf_iterator<char>());
+
+  auto stream = stringstream{contents};
+
+  vector<uint> mes;
+  for (string line; getline(stream, line, '\n');)
+    mes.emplace_back(atoi(line.c_str()));
+
+  cout << "part one: " << count_increases(mes, 1) << "\n";
+  cout << "part two: " << count_increases(mes, 3) << "\n";
+}
