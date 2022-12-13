043a71bcb945daf1ec2035b8fb281a4a557f33b1
Andriy Mykhaylyk <erp.lsf@gmail.com>
2022-12-13 22:18:54 +0100

    stash work

--- zig/src/2022/13.zig
+++ zig/src/2022/13.zig
@@ -25,16 +25,82 @@ const Node = union(enum) {
     value: usize,
     list: List,
 
-    pub fn compareOrder(self: *const Node, other: Node) bool {
-        _ = other;
-        _ = self;
-        return false;
+    pub fn init(allocator: Allocator) !Node {
+        return Node{ .list = List.init(allocator) };
+    }
+
+    pub fn buildNode(allocator: Allocator, line: []const u8) !Node {
+        var node = Node.init(allocator);
+        _ = line;
+        return node;
+    }
+
+    pub fn deinit(self: Node, allocator: Allocator) void {
+        if (self == Node.value) return;
+        for (self.list.items) |*item| {
+            item.deinit(allocator);
+        }
+        self.list.deinit();
+    }
+
+    pub fn compareOrder(allocator: Allocator, self: Node, other: Node) !?bool {
+        if (self == Node.value and other == Node.value) {
+            if (self.value < other.value) {
+                return true;
+            } else if (self.value > other.value) {
+                return false;
+            } else {
+                return null;
+            }
+        } else if (self == Node.list and other == Node.list) {
+            var i: usize = 0;
+            while (i < self.list.items.len and i < other.list.items.len) : (i += 1) {
+                if (compareOrder(allocator, self.list.items[i], other.list.items[i])) |result| {
+                    return result;
+                } else |_| {
+                    unreachable;
+                }
+            }
+            if (self.list.items.len < other.list.items.len) {
+                return true;
+            } else if (self.list.items.len > other.list.items.len) {
+                return false;
+            } else {
+                return null;
+            }
+        } else {
+            var nodeA = try upsert(allocator, self);
+            defer nodeA.deinit(allocator);
+            var nodeB = try upsert(allocator, other);
+            defer nodeB.deinit(allocator);
+            return compareOrder(allocator, nodeA, nodeB);
+        }
+        unreachable;
+    }
+
+    fn upsert(allocator: Allocator, node: Node) !Node {
+        if (node == Node.list) return node;
+
+        var list = List.init(allocator);
+        try list.append(Node{ .value = node.value });
+        return Node{ .list = list };
     }
 };
 
 fn solve(allocator: Allocator, input: []const u8) !Solution {
-    _ = allocator;
-    _ = input;
+    var it = std.mem.split(u8, input, "\n\n");
+    while (it.next()) |parts| {
+        if (parts.len == 0) continue;
+        var partsIt = std.mem.split(u8, parts, "\n");
+        var firstNode = try Node.buildNode(allocator, partsIt.next().?);
+        defer firstNode.deinit(allocator);
+
+        var secondNode = try Node.buildNode(allocator, partsIt.next().?);
+        defer secondNode.deinit(allocator);
+
+        const rightOrder = (try Node.compareOrder(allocator, firstNode, secondNode)).?;
+        _ = rightOrder;
+    }
     return Solution{ .part_1 = 0, .part_2 = 0 };
 }
 
