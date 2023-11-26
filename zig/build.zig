692846736328a02b921ac2ac7bcb307ed13ca62e
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-11-26 14:49:28 +0100

    update build.zig for new version and disable packages (for now)

--- zig/build.zig
+++ zig/build.zig
@@ -14,13 +14,13 @@ const Package = struct {
     tests: bool = false,
 };
 
-const packages = &[_]Package{
-    .{ .name = "aoc", .path = "src/aoc.zig" },
-    .{ .name = "pm", .path = "src/permute.zig", .tests = true },
-    .{ .name = "ug", .path = "src/ug.zig", .tests = true },
-};
+// const packages = &[_]Package{
+//     .{ .name = "aoc", .path = "src/aoc.zig" },
+//     .{ .name = "pm", .path = "src/permute.zig", .tests = true },
+//     .{ .name = "ug", .path = "src/ug.zig", .tests = true },
+// };
 
-pub fn build(b: *std.Builder) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -29,7 +29,7 @@ pub fn build(b: *std.Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -54,7 +54,9 @@ pub fn build(b: *std.Builder) void {
     var iter = dir.iterate();
     while (try iter.next()) |entry| {
         // std.debug.print("name: {s} kind: {}\n", .{entry.name, entry.kind});
-        if (entry.kind == .Directory) {
+        if (entry.kind == .directory) {
+            if (std.mem.startsWith(u8, entry.name, "disabled")) continue; // skip processing the whole folder if it's marked as "disabled"
+
             // entry is a year
             const year_dir_path = try std.fmt.allocPrint(allocator, "src/{s}", .{entry.name});
             defer allocator.free(year_dir_path);
@@ -67,7 +69,7 @@ pub fn build(b: *std.Builder) void {
 
             var year_iter = year_dir.iterate();
             while (try year_iter.next()) |day_entry| {
-                if (day_entry.kind == .File) {
+                if (day_entry.kind == .file) {
                     const year = try std.fmt.parseUnsigned(u16, entry.name, 10);
                     const day_part = std.mem.trimRight(u8, day_entry.name, ".zig");
                     const day = try std.fmt.parseUnsigned(u8, day_part, 10);
@@ -88,17 +90,20 @@ pub fn build(b: *std.Builder) void {
 
     for (solutions.items) |solution| {
         // std.debug.print("{d} {d} {s} {s}\n", solution);
-        const exe = b.addExecutable(solution.solution_name, solution.relative_path);
+        const exe = b.addExecutable(.{
+            .name = solution.solution_name,
+            .root_source_file = .{ .path = solution.relative_path },
+            .target = target,
+            .optimize = optimize,
+        });
 
-        for (packages) |package| {
-            exe.addPackagePath(package.name, package.path);
-        }
+        // for (packages) |package| {
+        //     exe.addPackagePath(package.name, package.path);
+        // }
 
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
-        exe.install();
+        b.installArtifact(exe);
 
-        const run_cmd = exe.run();
+        const run_cmd = b.addRunArtifact(exe);
         run_cmd.step.dependOn(b.getInstallStep());
         if (b.args) |args| {
             run_cmd.addArgs(args);
@@ -107,12 +112,14 @@ pub fn build(b: *std.Builder) void {
         const run_step = b.step(solution.solution_name, solution.solution_name);
         run_step.dependOn(&run_cmd.step);
 
-        const exe_tests = b.addTest(solution.relative_path);
-        for (packages) |package| {
-            exe_tests.addPackagePath(package.name, package.path);
-        }
-        exe_tests.setTarget(target);
-        exe_tests.setBuildMode(mode);
+        const exe_tests = b.addTest(.{
+            .root_source_file = .{ .path = solution.relative_path },
+            .target = target,
+            .optimize = optimize,
+        });
+        // for (packages) |package| {
+        //     exe_tests.addPackagePath(package.name, package.path);
+        // }
 
         const one_test_step = b.step(solution.solution_test_name, "Run unit tests");
         one_test_step.dependOn(&exe_tests.step);
@@ -125,22 +132,22 @@ pub fn build(b: *std.Builder) void {
     }
 
     // permute tests
-    for (packages) |package| {
-        if (!package.tests) continue;
-        const package_test = b.addTest(package.path);
-        for (packages) |sub_package| {
-            // if (package.name == sub_package.name) continue;
-            package_test.addPackagePath(sub_package.name, sub_package.path);
-        }
-        package_test.setTarget(target);
-        package_test.setBuildMode(mode);
+    // for (packages) |package| {
+    //     if (!package.tests) continue;
+    //     const package_test = b.addTest(package.path);
+    //     for (packages) |sub_package| {
+    //         // if (package.name == sub_package.name) continue;
+    //         package_test.addPackagePath(sub_package.name, sub_package.path);
+    //     }
+    //     package_test.setTarget(target);
+    //     package_test.setBuildOptimize(optimize);
 
-        const pts_name = try std.fmt.allocPrint(allocator, "{s}-test", .{package.name});
-        const pts_desc = try std.fmt.allocPrint(allocator, "{s}-test", .{package.name});
-        defer allocator.free(pts_name);
-        defer allocator.free(pts_desc);
+    //     const pts_name = try std.fmt.allocPrint(allocator, "{s}-test", .{package.name});
+    //     const pts_desc = try std.fmt.allocPrint(allocator, "{s}-test", .{package.name});
+    //     defer allocator.free(pts_name);
+    //     defer allocator.free(pts_desc);
 
-        const package_test_step = b.step(pts_name, pts_desc);
-        package_test_step.dependOn(&package_test.step);
-    }
+    //     const package_test_step = b.step(pts_name, pts_desc);
+    //     package_test_step.dependOn(&package_test.step);
+    // }
 }
