1acaf0464e985a97c3dc10588a7fd7b5e70872ea
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-12-09 19:00:30 +0100

    optimize 1

--- zig/src/2023/1.zig
+++ zig/src/2023/1.zig
@@ -33,25 +33,68 @@ pub fn solve(input: []const u8) Solution {
     return .{ .p1 = p1sum, .p2 = p2sum };
 }
 
-const searchForwards = std.mem.indexOfAny;
-const searchBackwards = std.mem.lastIndexOfAny;
-
 const Direction = enum {
     Forwards,
     Backwards,
 };
 
-pub fn findNumber(comptime direction: Direction, input: []const u8) usize {
-    const searchFn = if (comptime direction == .Forwards) searchForwards else searchBackwards;
-    const i = searchFn(u8, input, "123456789").?;
-    return std.fmt.parseUnsigned(usize, input[i .. i + 1], 10) catch unreachable;
+const Mode = enum {
+    Simple,
+    Complex,
+};
+
+pub fn findNumber(comptime mode: Mode, comptime direction: Direction, input: []const u8) usize {
+    const searchFn = if (comptime direction == .Forwards) std.mem.indexOfAny else std.mem.lastIndexOfAny;
+    if (comptime mode == .Simple) {
+        const maybeIndex = searchFn(u8, input, "123456789");
+        if (maybeIndex) |i|
+            return std.fmt.parseUnsigned(usize, input[i .. i + 1], 10) catch unreachable
+        else
+            return 0;
+    } else {
+        // std.debug.print("searching in: {s}\n", .{input});
+        const wordSearchFn = if (comptime direction == .Forwards) std.mem.indexOf else std.mem.lastIndexOf;
+        const maybeDigitIndex = searchFn(u8, input, "123456789");
+        const fillValue = if (comptime direction == .Forwards) std.math.maxInt(usize) else std.math.minInt(usize);
+        const op = if (comptime direction == .Forwards) std.builtin.ReduceOp.Min else std.builtin.ReduceOp.Max;
+        var is: [words.len]usize = [_]usize{fillValue} ** words.len;
+        for (words, 0..) |word, i| { // NOTE: maybe inline it?
+            const maybeWordIndex = wordSearchFn(u8, input, word);
+            if (maybeWordIndex) |index| is[i] = index;
+        }
+        const v: @Vector(words.len, usize) = is;
+        const wI = @reduce(op, v);
+        const wNumber: usize = for (is, 1..) |i, n| {
+            if (i == wI) break n;
+        } else fillValue;
+        if (maybeDigitIndex) |dI| {
+            const i = if (comptime direction == .Forwards)
+                @min(dI, wI)
+            else
+                @max(dI, wI);
+
+            if (i == wI) {
+                if (wI == fillValue) return std.fmt.parseUnsigned(usize, input[dI .. dI + 1], 10) catch unreachable else return wNumber;
+            } else {
+                return std.fmt.parseUnsigned(usize, input[dI .. dI + 1], 10) catch unreachable;
+            }
+        }
+        if (wI == fillValue) return 0 else return wNumber;
+    }
 }
 
 pub fn parseLine(line: []const u8) struct { usize, usize } {
-    const p1fn = findNumber(.Forwards, line);
-    const p2fn = findNumber(.Backwards, line);
+    const p1fn = findNumber(.Simple, .Forwards, line);
+    const p1sn = findNumber(.Simple, .Backwards, line);
 
-    return .{ p1fn * 10 + p2fn, 0 };
+    // std.debug.print("{d} <- {s} -> {d}\n", .{ p1fn, line, p1sn });
+
+    const p2fn = findNumber(.Complex, .Forwards, line);
+    const p2sn = findNumber(.Complex, .Backwards, line);
+
+    // std.debug.print("{d} <- {s} -> {d}\n", .{ p2fn, line, p2sn });
+
+    return .{ p1fn * 10 + p1sn, p2fn * 10 + p2sn };
 }
 
 test "Part 1" {
@@ -65,6 +108,41 @@ test "Part 1" {
     try std.testing.expectEqual(@as(usize, 142), solve(input).p1);
 }
 
+test "Part 2" {
+    const input =
+        \\two1nine
+        \\eightwothree
+        \\abcone2threexyz
+        \\xtwone3four
+        \\4nineeightseven2
+        \\zoneight234
+        \\7pqrstsixteen
+    ;
+
+    try std.testing.expectEqual(@as(usize, 281), solve(input).p2);
+}
+
+test "freak tests" {
+    // const input =
+    //     \\threeight
+    //     \\sevenine
+    //     \\oneight
+    //     \\zoneight234
+    // ;
+
+    try std.testing.expectEqual(@as(usize, 0), solve("threeight").p1);
+    try std.testing.expectEqual(@as(usize, 38), solve("threeight").p2);
+    try std.testing.expectEqual(@as(usize, 88), solve("threight").p2);
+
+    try std.testing.expectEqual(@as(usize, 0), solve("sevenine").p1);
+    try std.testing.expectEqual(@as(usize, 79), solve("sevenine").p2);
+
+    try std.testing.expectEqual(@as(usize, 0), solve("oneight").p1);
+    try std.testing.expectEqual(@as(usize, 18), solve("oneight").p2);
+
+    try std.testing.expectEqual(@as(usize, 33), solve("3kbklxmh").p2);
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -74,58 +152,9 @@ pub fn main() !void {
     defer std.process.argsFree(allocator, args);
 
     const buffer = try utils.readFile(allocator);
+    defer allocator.free(buffer);
+    const s = solve(buffer);
 
-    var digits = std.ArrayList(usize).init(allocator);
-    defer digits.deinit();
-
-    var sum: usize = 0;
-
-    var it = std.mem.splitScalar(u8, buffer, '\n');
-    while (it.next()) |line| {
-        var i: usize = 0;
-        var wSize: usize = 1;
-
-        while (i + wSize <= line.len) : (wSize += 1) {
-            const end = @min(i + wSize, line.len);
-            const slice = line[i..end];
-            // std.debug.print("i: {d}, wSize: {d}, end: {d}\n", .{ i, wSize, end });
-            // std.debug.print("slice: {s}\n", .{slice});
-            if (wSize == 1) {
-                const digit = std.fmt.parseUnsigned(u8, slice, 10) catch 0;
-                if (digit != 0) {
-                    try digits.append(digit);
-                    wSize = 0;
-                    i += 1;
-                }
-            } else {
-                const digit = for (words, 1..) |word, d| {
-                    if (wSize < word.len) continue;
-                    if (std.mem.indexOf(u8, slice, word)) |p| {
-                        wSize = 0;
-                        i += p + word.len - 1;
-                        break d;
-                    }
-                } else 0;
-                if (digit != 0) {
-                    try digits.append(digit);
-                } else {
-                    const d = std.fmt.parseUnsigned(u8, slice[slice.len - 1 ..], 10) catch 0;
-                    if (d != 0) {
-                        try digits.append(d);
-                        wSize = 0;
-                        i += slice.len;
-                    }
-                }
-            }
-        }
-
-        if (digits.items.len == 0) continue;
-        std.debug.print("line: {s}, d1: {d}, d2: {d}\n", .{ line, digits.items[0], digits.items[digits.items.len - 1] });
-        const number = digits.items[0] * 10 + digits.items[digits.items.len - 1];
-        sum += number;
-        digits.clearAndFree();
-    }
-    std.debug.print("sum: {d}\n", .{sum});
-
-    allocator.free(buffer);
+    std.debug.print("Part 1: {d}\n", .{s.p1});
+    std.debug.print("Part 2: {d}\n", .{s.p2});
 }
