761500053b93e989b6dfb6f6a642268ce93e9b3f
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-12-14 14:29:11 +0100

    solve 14.2

--- zig/src/2023/14.zig
+++ zig/src/2023/14.zig
@@ -32,13 +32,68 @@ pub fn solve(allocator: std.mem.Allocator, input: []u8) !Solution {
     tilt(&map, .south);
     tilt(&map, .east);
 
-    // const cycleCount = 1000000000;
-    // for (0..cycleCount) |_| {}
+    var cycleMap = std.StringArrayHashMap(void).init(allocator);
+    defer {
+        var it = cycleMap.iterator();
+        while (it.next()) |kv| allocator.free(kv.key_ptr.*);
+        cycleMap.deinit();
+    }
+    var weightsList = std.ArrayList(usize).init(allocator);
+    defer weightsList.deinit();
+
+    weightsList.append(calcNorthLoad(map)) catch unreachable;
+
+    const string = stringify(allocator, map);
+    cycleMap.put(string, {}) catch unreachable;
+
+    const cycleCount = 1000000000 - 1;
+    var loopAt: usize = 0;
+    var loopStartsAt: usize = 0;
+    for (0..cycleCount) |c| {
+        cycle(&map);
+        const state = stringify(allocator, map);
+        if (cycleMap.get(state)) |_| {
+            loopStartsAt = cycleMap.getIndex(state).?;
+            allocator.free(state);
+            loopAt = c + 2;
+            break;
+        } else {
+            const newWeight = calcNorthLoad(map);
+            weightsList.append(newWeight) catch unreachable;
+            cycleMap.put(state, {}) catch unreachable;
+        }
+    }
+    // std.debug.print("loopAt, startsAt: {d}, {d}\n", .{ loopAt, loopStartsAt });
+    // std.debug.print("len: {d}, list: {any}\n", .{ weightsList.items.len, weightsList.items });
+    const loop = weightsList.items[loopStartsAt..];
+    // std.debug.print("len: {d}, loop: {any}\n", .{ loop.len, loop });
+    const leftoverFullLoops: usize = cycleCount - loopStartsAt;
+    const leftoverLoops: usize = leftoverFullLoops % loop.len;
+    const finalWeight: usize = loop[leftoverLoops];
+    // std.debug.print("final weight: {d}\n", .{finalWeight});
 
     // printMap(map);
     // std.debug.print("\n", .{});
 
-    return .{ .p1 = firstNorthTilt, .p2 = 0 };
+    return .{ .p1 = firstNorthTilt, .p2 = finalWeight };
+}
+
+pub fn stringify(allocator: std.mem.Allocator, map: []const []const u8) []u8 {
+    const totalLength: usize = map.len * map[0].len;
+    var string = allocator.alloc(u8, totalLength) catch unreachable;
+    for (1..map.len) |r| {
+        const sliceSize: usize = map[r].len;
+        @memcpy(string[(r - 1) * sliceSize .. r * sliceSize], map[r]);
+    }
+
+    return string;
+}
+
+pub fn cycle(map: *[]const []u8) void {
+    tilt(map, .north);
+    tilt(map, .west);
+    tilt(map, .south);
+    tilt(map, .east);
 }
 
 pub fn calcNorthLoad(map: []const []const u8) usize {
@@ -68,28 +123,36 @@ const Direction = enum {
 };
 
 pub fn tilt(map: *[]const []u8, comptime direction: Direction) void {
+    // std.debug.print("direction: {s}\n", .{@tagName(direction)});
     var m = map.*;
     const posLimit = switch (comptime direction) {
         .north => m.len, // moving up-to-down, to the length of column
         .south => 0, // down-to-up, to zero
-        .west => 0, // right-to-left, to zero
-        .east => m[0].len, // left-to-right, to the length of row
+        .west => m[0].len, // right-to-left, to the length of the row
+        .east => 0, // left-to-right, to zero
     };
     const axisLimit = switch (comptime direction) {
         .north, .south => m[0].len, // tilt up or down for each column
         .west, .east => m.len, // tilt left or right for each row
     };
-    // std.debug.print("rowCount: {d}", .{rowCount});
+    // std.debug.print("axisLimit: {d}\n", .{axisLimit});
 
     for (0..axisLimit) |axis| {
+        var iteratedOnZero: bool = false; // HACK:TODO: ugly
         var freePos: ?usize = null;
         var i: usize = switch (comptime direction) {
             .north => 0, // start from beginning of the column
             .south => m.len - 1, // start from the end of the column
-            .west => m[0].len - 1, // start from the end of the row
-            .east => 0, // start from the end ofthe row
+            .west => 0, // start from the beginning of the row
+            .east => m[0].len - 1, // start from the end of the row
         };
-        while (i != posLimit) {
+        while (true) {
+            if (posLimit != 0 and i == posLimit) break; // break for going left-to-right and up-to-down
+            if (posLimit == 0 and i == posLimit) iteratedOnZero = true;
+            // switch (comptime direction) {
+            //     .north, .south => std.debug.print("objIndex: [{d}][{d}]\n", .{ i, axis }),
+            //     .east, .west => std.debug.print("objIndex: [{d}][{d}]\n", .{ axis, i }),
+            // }
             // std.debug.print("r: {d}\n", .{r});
             // std.debug.print("char {c} at [{d}][{d}]\n", .{ m[r][column], r, column });
 
@@ -102,8 +165,8 @@ pub fn tilt(map: *[]const []u8, comptime direction: Direction) void {
                 '#' => { // cube-shaped rock (wall)
                     freePos = null; // reset the free position, as rocks can't move past the wall
                     switch (comptime direction) {
-                        .north, .east => i += 1,
-                        .south, .west => i -= 1,
+                        .north, .west => i += 1,
+                        .south, .east => i -|= 1,
                     }
                 },
                 '.' => { // free space
@@ -111,8 +174,8 @@ pub fn tilt(map: *[]const []u8, comptime direction: Direction) void {
                         freePos = i; // only record the row the first time we see free space
                     }
                     switch (comptime direction) {
-                        .north, .east => i += 1,
-                        .south, .west => i -= 1,
+                        .north, .west => i += 1,
+                        .south, .east => i -|= 1,
                     }
                 },
                 'O' => { // movable rock
@@ -127,18 +190,19 @@ pub fn tilt(map: *[]const []u8, comptime direction: Direction) void {
                         freePos = null; // there's no more free space, we need to search for it again
 
                         switch (comptime direction) { // start searching from the next row after the one we placed our rock into
-                            .north, .east => i = fr + 1,
-                            .south, .west => i = fr - 1,
+                            .north, .west => i = fr + 1,
+                            .south, .east => i = fr -| 1,
                         }
                     } else {
                         switch (comptime direction) {
-                            .north, .east => i += 1,
-                            .south, .west => i -= 1,
+                            .north, .west => i += 1,
+                            .south, .east => i -|= 1,
                         }
                     }
                 },
                 else => unreachable,
             }
+            if (posLimit == 0 and i == 0 and iteratedOnZero == true) break; // break for reverse direction
         }
     }
 }
