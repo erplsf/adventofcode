945db4b2f008834efe2e1c5a0f1b223fd1f579ef
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-11-30 12:32:14 +0100

    change structure for aoc

--- zig/src/2024/01/main.zig
+++ zig/src/2024/01/main.zig
@@ -3,35 +3,26 @@
 //! is to delete this file and start with root.zig instead.
 const std = @import("std");
 
+pub fn solve(allocator: std.mem.Allocator, input: []const u8) !struct { p1: usize, p2: usize } {
+    _ = allocator;
+    _ = input;
+    return .{ .p1 = 42, .p2 = 84 };
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const answers = try solve(allocator, "");
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
+    std.debug.print("Part 1: {d}\n", .{answers.p1});
+    std.debug.print("Part 2: {d}\n", .{answers.p2});
 }
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    const answers = try solve(std.testing.allocator, "");
 
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
+    try std.testing.expectEqual(@as(usize, 42), answers.p1);
+    try std.testing.expectEqual(@as(usize, 84), answers.p2);
 }
