0757bd57c48f74b051d54a2a5d22a2e5963c6947
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-11-30 12:41:30 +0100

    add reading file

--- zig/src/2024/01/main.zig
+++ zig/src/2024/01/main.zig
@@ -14,7 +14,22 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const answers = try solve(allocator, "");
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const path = try std.fs.realpathAlloc(allocator, args[1]);
+    defer allocator.free(path);
+
+    const file = try std.fs.openFileAbsolute(path, .{ .mode = .read_only });
+    defer file.close();
+
+    const size = (try file.stat()).size;
+    const buffer = try allocator.alloc(u8, size);
+    defer allocator.free(buffer);
+
+    try file.reader().readNoEof(buffer);
+
+    const answers = try solve(allocator, buffer);
 
     std.debug.print("Part 1: {d}\n", .{answers.p1});
     std.debug.print("Part 2: {d}\n", .{answers.p2});
