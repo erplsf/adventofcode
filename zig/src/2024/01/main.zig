136b65e607fba2412da3d58e1fba4ba6a195dd3a
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-12-01 11:13:30 +0100

    2024-01-01

--- zig/src/2024/01/main.zig
+++ zig/src/2024/01/main.zig
@@ -1,9 +1,37 @@
 const std = @import("std");
 
 pub fn solve(allocator: std.mem.Allocator, input: []const u8) !struct { p1: usize, p2: usize } {
-    _ = allocator;
-    _ = input;
-    return .{ .p1 = 42, .p2 = 84 };
+    var lines_it = std.mem.tokenizeScalar(u8, input, '\n');
+
+    var left_numbers = std.ArrayList(usize).init(allocator);
+    defer left_numbers.deinit();
+
+    var right_numbers = std.ArrayList(usize).init(allocator);
+    defer right_numbers.deinit();
+
+    while (lines_it.next()) |line| {
+        var parts_it = std.mem.tokenizeScalar(u8, line, ' ');
+        const left_number = parts_it.next().?;
+        const right_number = parts_it.next().?;
+
+        try left_numbers.append(try std.fmt.parseUnsigned(usize, left_number, 10));
+        try right_numbers.append(try std.fmt.parseUnsigned(usize, right_number, 10));
+    }
+
+    const sort_func = std.sort.asc(usize);
+
+    std.mem.sort(usize, left_numbers.items, {}, sort_func);
+    std.mem.sort(usize, right_numbers.items, {}, sort_func);
+
+    std.debug.assert(left_numbers.items.len == right_numbers.items.len);
+
+    var sum: usize = 0;
+    for (0..left_numbers.items.len) |i| {
+        const diff: usize = @abs(@as(isize, @intCast(left_numbers.items[i])) - @as(isize, @intCast(right_numbers.items[i])));
+        sum += diff;
+    }
+
+    return .{ .p1 = sum, .p2 = 84 };
 }
 
 pub fn main() !void {
@@ -33,8 +61,16 @@ pub fn main() !void {
 }
 
 test "simple test" {
-    const answers = try solve(std.testing.allocator, "");
+    const input =
+        \\3   4
+        \\4   3
+        \\2   5
+        \\1   3
+        \\3   9
+        \\3   3
+    ;
+    const answers = try solve(std.testing.allocator, input);
 
-    try std.testing.expectEqual(@as(usize, 42), answers.p1);
-    try std.testing.expectEqual(@as(usize, 84), answers.p2);
+    try std.testing.expectEqual(@as(usize, 11), answers.p1);
+    // try std.testing.expectEqual(@as(usize, 84), answers.p2);
 }
