c2431a5ccb78689316fa651ca6d39b9c75a6a794
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-11-14 16:32:34 +0100

    scaffold

--- zig/src/2015/1.zig
+++ zig/src/2015/1.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+}
+
+fn solve(allocator: Allocator, input: []const u8) !.{ usize, usize } {
+    _ = allocator;
+    _ = input;
+    return .{ 0, 0 };
+}
+
+test {
+    const p1, _ = try solve("(())");
+    try std.testing.expectEqual(p1, 0);
 }
