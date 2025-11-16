9162b47e7e93842845c68f22390afa5b1669a09e
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-11-16 13:08:10 +0100

    add basic tests

--- zig/src/2015/1.zig
+++ zig/src/2015/1.zig
@@ -5,14 +5,57 @@ pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 }
 
-fn solve(allocator: Allocator, input: []const u8) !struct { usize, usize } {
-    _ = allocator;
-    _ = input;
-    return .{ 0, 0 };
+fn solve(input: []const u8) struct { isize, usize } {
+    var level: isize = 0;
+    for (input) |c| {
+        switch (c) {
+            '(' => level += 1,
+            ')' => level -= 1,
+            else => unreachable,
+        }
+    }
+    return .{ level, 0 };
 }
 
-test {
-    const allocator = std.testing.allocator;
-    const p1, _ = try solve(allocator, "(())");
-    try std.testing.expectEqual(p1, 0);
+test "part 1" {
+    {
+        const p1, _ = solve("(())");
+        try std.testing.expectEqual(0, p1);
+    }
+    {
+        const p1, _ = solve("()()");
+        try std.testing.expectEqual(0, p1);
+    }
+    {
+        const p1, _ = solve("(((");
+        try std.testing.expectEqual(3, p1);
+    }
+    {
+        const p1, _ = solve("(()(()(");
+        try std.testing.expectEqual(3, p1);
+    }
+    {
+        const p1, _ = solve("))(((((");
+        try std.testing.expectEqual(3, p1);
+    }
+    {
+        const p1, _ = solve("))(((((");
+        try std.testing.expectEqual(3, p1);
+    }
+    {
+        const p1, _ = solve("())");
+        try std.testing.expectEqual(-1, p1);
+    }
+    {
+        const p1, _ = solve("))(");
+        try std.testing.expectEqual(-1, p1);
+    }
+    {
+        const p1, _ = solve(")))");
+        try std.testing.expectEqual(-3, p1);
+    }
+    {
+        const p1, _ = solve(")())())");
+        try std.testing.expectEqual(-3, p1);
+    }
 }
