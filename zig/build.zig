3d8c8f26fd6fea946dc31cc1720481f471796f50
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-11-16 09:38:22 +0100

    add global test step

--- zig/build.zig
+++ zig/build.zig
@@ -73,6 +73,9 @@ pub fn build(b: *std.Build) !void {
     // If neither case applies to you, feel free to delete the declaration you
     // don't need and to put everything under a single module.
 
+    // A top level step for running all tests.
+    const test_step = b.step("test", "Run all tests");
+
     inline for (files, 0..) |file, i| {
         const filename = std.fmt.comptimePrint("src/{d}/{d}.zig", .{ file.year, file.day });
         const exe_name = std.fmt.comptimePrint("{d}-{d}", .{ file.year, file.day });
@@ -133,34 +136,21 @@ pub fn build(b: *std.Build) !void {
         if (b.args) |args| {
             run_cmd.addArgs(args);
         }
+
+        // Creates an executable that will run `test` blocks from the executable's
+        // root module. Note that test executables only test one module at a time,
+        // hence why we have to create two separate ones.
+        const exe_tests = b.addTest(.{
+            .root_module = exe.root_module,
+        });
+
+        // A run step that will run the test executable.
+        const run_exe_tests = b.addRunArtifact(exe_tests);
+
+        // dependOn can be called multiple times and since the two run steps do not depend on one another, this will
+        // make the two of them run in parallel.
+        test_step.dependOn(&run_exe_tests.step);
     }
-
-    // // Creates an executable that will run `test` blocks from the provided module.
-    // // Here `mod` needs to define a target, which is why earlier we made sure to
-    // // set the releative field.
-    // const mod_tests = b.addTest(.{
-    //     .root_module = mod,
-    // });
-
-    // // A run step that will run the test executable.
-    // const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    // // Creates an executable that will run `test` blocks from the executable's
-    // // root module. Note that test executables only test one module at a time,
-    // // hence why we have to create two separate ones.
-    // const exe_tests = b.addTest(.{
-    //     .root_module = exe.root_module,
-    // });
-
-    // // A run step that will run the second test executable.
-    // const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    // // A top level step for running all tests. dependOn can be called multiple
-    // // times and since the two run steps do not depend on one another, this will
-    // // make the two of them run in parallel.
-    // const test_step = b.step("test", "Run tests");
-    // test_step.dependOn(&run_mod_tests.step);
-    // test_step.dependOn(&run_exe_tests.step);
 
     // // Just like flags, top level steps are also listed in the `--help` menu.
     // //
